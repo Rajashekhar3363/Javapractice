779288b14cf025e08505397945ad83075a68f8a4
Rajashekhar <rajashekhar.naragoni360@gmail.com>
2022-06-24 12:08:28 +0530

    commit

--- Javaproject1/src/Oppsconcepts/Employee.java
+++ Javaproject1/src/Oppsconcepts/Employee.java
@@ -3,29 +3,27 @@ public class Employee
 {
 	String name;
 	int id;
-	Car car;
+	
 	Bike1 bike;
 	
-	Employee(String name,int id,Car car,Bike1 b)
+	Employee(String name,int id,Bike1 b)
 	{
 		this.name=name;
 		this.id=id;
-		this.car=car;
+		
 		this.bike=b;
 	}
 public static void main(String[] args)
 {
-		Car c=new Car("TATA","TS456564PS","BIRLA");
+		
 		Bike1 b=new Bike1("Enfield","S45877g55",350,300000);
-		Employee e=new Employee("Sundar",14562,c,b);
+		Employee e=new Employee("Sundar",14562,b);
 		
 	    System.out.println("Employee Details");
 		System.out.println("Empoyee name:"+e.name);
 		System.out.println(e.id);
 		System.out.println("Car related Details");
-		System.out.println(e.car.name);
-		System.out.println(e.car.egno);
-		System.out.println(e.car.comp);
+		
 		System.out.println("Bike related Details");
 		System.out.println(e.bike.name);
 		System.out.println(e.bike.eno);
